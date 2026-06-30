a07ba7a2cb8052891b68073cd6ed4f0f709338e2
djonquest <djonquest@gmail.com>
2026-06-30 18:58:41 -0300

    Fix logic bugs in list functions
    Fix critical logic error in listarLivro() and listarUsuario() functions where the for loop was incorrectly nested inside the if(total == 0) check. The loop should only execute when books/users exist. Also fixed typo 'cadatrado' to 'cadastrado', added Windows UTF-8 console support, added proper return statements, and improved code formatting.

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<windows.h>
 
 
 #define MAX_LIVROS 100
@@ -10,44 +11,58 @@ void listarUsuario(int codigo[], char nome[][50], char telefone[][50], char emai
 void listarLivro(int codigo[], char titulo[][50], char autor[][50], int categoria[], int ano[], int quantidade[], int quant_disponivel[], int total);
 
 int main (){
+    SetConsoleOutputCP(CP_UTF8);
+
+    int i, j, 
+
+    do
+    {
+        /* code */
+    } while (opcao != 0);
+    
 
 return 0;
 
 }
 
     // Função Listar Livro
-    void listarLivro(int codigo[], char titulo[][50], char autor[][50], int categoria[], int ano[], int quantidade[], int quant_disponivel[], int total)
-    {
+    void listarLivro(int codigo[], char titulo[][50], char autor[][50], int categoria[], int ano[], int quantidade[], int quant_disponivel[], int total){
 
         if (total == 0){
-            printf("--- Nenhum livro cadatrado ---\n");
-
-            for (int i = 0; i < total; i++){
-                printf("Código: %d\n", codigo[i]);
-                printf("Titulo: %s\n", titulo[i]);
-                printf("Autor: %s\n", autor[i]);
-                printf("Categoria: %d", categoria[i]);
-                printf("Ano: %d", ano[i]);
-                printf("Quantidade: %d", quantidade[i]);
-                printf("Quantidade Disponivel: %d", quant_disponivel[i]);
-                printf("------------------------------");
-            }
+            printf("--- Nenhum livro cadastrado! ---\n");
+            return 0;
         }
+
+    printf("--- Lista de livros cadastrados ---");
         
-    }
+        for (int i = 0; i < total; i++){
+            printf("Código: %d\n", codigo[i]);
+            printf("Titulo: %s\n", titulo[i]);
+            printf("Autor: %s\n", autor[i]);
+            printf("Categoria: %d", categoria[i]);
+            printf("Ano: %d", ano[i]);
+            printf("Quantidade: %d", quantidade[i]);
+            printf("Quantidade Disponivel: %d", quant_disponivel[i]);
+            printf("------------------------------");
+        }
+}
 
     // Função Listar Usuario
     void listarUsuario(int codigo[], char nome[][50], char telefone[][50], char email[], int total){
 
         if (total == 0){
             printf("--- Nenhum usuário cadatrado ---\n");
+            return 0;
 
-            for (int i = 0; i < total; i++){
-                printf("Código: %d\n", codigo[i]);
-                printf("Nome: %s\n", nome[i]);
-                printf("Telefone: %s\n", telefone[i]);
-                printf("Email: %c", email[i]);
-                printf("------------------------------");
-            }
+        }
+
+    printf("--- Lista de usuários cadastrados ---");
+
+        for (int i = 0; i < total; i++){
+            printf("Código: %d\n", codigo[i]);
+            printf("Nome: %s\n", nome[i]);
+            printf("Telefone: %s\n", telefone[i]);
+            printf("Email: %c", email[i]);
+            printf("------------------------------");
         }
 }
